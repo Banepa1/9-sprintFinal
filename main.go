fdd98f65aef104cc46d205837fd10f2d83d57d07
Владыкин Никита <vld.kolkol@mail.ru>
2026-03-12 20:35:12 +0300

    first-iteration

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -12,29 +15,113 @@ const (
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
 	// ваш код здесь
+	if size <= 0 {
+		return nil
+	}
+
+	rnd := rand.New(rand.NewSource(time.Now().Unix()))
+	data := make([]int, size)
+
+	for i := range data {
+		randomNumber := rnd.Intn(size)
+		data[i] = randomNumber
+	}
+
+	return data
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+	if len(data) == 1 {
+		return data[0]
+	}
+
+	max := data[0]
+
+	for _, v := range data {
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+	if len(data) == 1 {
+		return data[0]
+	}
+	if len(data) < 8 {
+		return maximum(data)
+	}
+
+	sliceOfMaximums :=make([]int, CHUNKS)
+	sizeOneSlice := len(data) / CHUNKS
+	max := data[0]
+
+	var wg sync.WaitGroup
+
+	wg.Add(CHUNKS)
+	for i := 0; i < CHUNKS; i++ {
+		startOfSlice := i*sizeOneSlice
+		endOfSlice := i*sizeOneSlice + sizeOneSlice
+		oneSlice := data[startOfSlice:endOfSlice]
+
+		if i == CHUNKS - 1 {
+			oneSlice = data[startOfSlice:]
+		}
+
+		go func(data []int){
+			defer wg.Done()
+			index := i
+
+			for _, v := range data {
+				if v > max {
+					max = v
+				}
+			}
+
+			sliceOfMaximums[index] = max
+		}(oneSlice)
+	}
+	wg.Wait()
+	max = sliceOfMaximums[0]
+	for _, v := range sliceOfMaximums {
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
+	data := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
 	// ваш код здесь
+	start := time.Now()
+	max := maximum(data)
+	elapsed := time.Since(start)
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed.Microseconds())
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
 	// ваш код здесь
+	start = time.Now()
+	max = maxChunks(data)
+	elapsed = time.Since(start)
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed.Microseconds())
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,71 @@
 package main
 
 // Пишите тесты в этом файле
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateRandomElements(t *testing.T) {	
+	assert.Nil(t, generateRandomElements(0))
+	assert.Nil(t, generateRandomElements(-1))
+
+	len := 6
+	data := generateRandomElements(len)
+	assert.Len(t, data, len)
+}
+
+func TestMaximum(t *testing.T) {
+	var nilSlice []int
+	assert.Zero(t, maximum(nilSlice))
+
+	zeroLenSlice := make([]int, 0)
+	assert.Zero(t, maximum(zeroLenSlice))
+
+	max := 6
+
+	var oneElementSlice = []int{max}
+	assert.Equal(t, max, maximum(oneElementSlice))
+
+	var firstMaxSlice = []int{max, 4, 3, 2}
+	assert.Equal(t, max, maximum(firstMaxSlice))
+
+	var endMaxSlice = []int{3, 4, 3, max}
+	assert.Equal(t, max, maximum(endMaxSlice))
+
+	var equalElementsSlice = []int{max, max, max, max, max, max, max, max}
+	assert.Equal(t, max, maximum(equalElementsSlice))
+}
+
+func TestMaxChunks(t *testing.T) {
+	var nilSlice []int
+	assert.Zero(t, maxChunks(nilSlice))
+
+	zeroLenSlice := make([]int, 0)
+	assert.Zero(t, maxChunks(zeroLenSlice))
+
+	max := 300
+
+	var oneElementSlice = []int{max}
+	assert.Equal(t, max, maxChunks(oneElementSlice))
+
+	var lessEightSlice = []int{6, 10, 200, 100, max}
+	assert.Equal(t, max, maxChunks(lessEightSlice))
+
+	var eightSlice = []int{7, 4, 3, 3, max, 23, 254, 10}
+	assert.Equal(t, max, maxChunks(eightSlice))
+
+	var notDivisibleBy8Slice = []int{7, 4, 3, 3, max, 23, 254, 10, 15}
+	assert.Equal(t, max, maxChunks(notDivisibleBy8Slice))
+
+	var equalElementsSlice = []int{max, max, max, max, max, max, max, max, max, max ,max}
+	assert.Equal(t, max, maxChunks(equalElementsSlice))
+
+	bigSlice := make([]int, 1_000_000)
+	for i := range bigSlice {
+		bigSlice[i] = i
+	}
+
+	assert.Equal(t, 999_999, maxChunks(bigSlice))
+}
